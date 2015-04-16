b5dbdb756faac845d5bbe911d48da5aab5e1bcec
Amiel Reveche <amielreveche@gmail.com>
2015-04-16 21:57:14 +0800

    routes update

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,7 +4,7 @@ class PageController < ApplicationController
 
 	def index
 
-		@sub_uri = "ports/"
+		@sub_uri = ""
 		@ports = Poi.order("id").all()
 		@images = Array.new(@ports.length)
 		@infos = Array.new(@ports.length)
@@ -14,11 +14,11 @@ class PageController < ApplicationController
 
 		@categories.each do |category|
 			if category.image != nil
-				@category_markers[category.id] = asset_url("assets/" + @sub_uri + "category_markers/" + category.image)
-				@category_filters[category.id] = asset_url("assets/" + @sub_uri + "category_markers/filter_" + category.image)
+				@category_markers[category.id] = asset_url(@sub_uri + "assets/category_markers/" + category.image)
+				@category_filters[category.id] = asset_url(@sub_uri + "assets/category_markers/filter_" + category.image)
 			else
-				@category_markers[category.id] = asset_url("assets/" + @sub_uri + "category_markers/default.png")
-				@category_filters[category.id] = asset_url("assets/" + @sub_uri + "category_markers/filter_default.png")
+				@category_markers[category.id] = asset_url(@sub_uri + "assets/category_markers/default.png")
+				@category_filters[category.id] = asset_url(@sub_uri + "assets/category_markers/filter_default.png")
 			end
 		end
 
@@ -26,6 +26,27 @@ class PageController < ApplicationController
 			@images[i] = Image.where(poi_id: port.id)
 			@infos[i] = Details.where(poi_id: port.id)
 		end
+	end
+
+	def getRoutes
+		@routeData = Hash.new
+		@routeData["params"] = params
+		@routeData["routeList"] = Array.new(1)
+
+		@route1 = Hash.new
+		@route1["id"] = 1
+		@route1["name"] = "Direct"
+		@route1["departure"] = "4:00"
+		@route1["arrival"] = "5:30"
+		@route1["flightPath"]
+
+		@routeData["routeList"][0] = @route1
+
+		render json: @routeData
+		# respond_to do |format|
+  #     format.html 
+  #     format.json { render json: @routeData }
+  #   end
 	end
 	
 end
